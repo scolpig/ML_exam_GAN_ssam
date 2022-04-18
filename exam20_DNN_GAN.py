a856157bc6861cb5eaf21b3559e7b6819aeb6217
scolpig <scolpig@gmail.com>
2022-04-18 11:50:38 +0900

    DNN_GAN 02

--- exam20_DNN_GAN.py
+++ exam20_DNN_GAN.py
@@ -26,7 +26,60 @@ generator.add(Dense(784, activation='tanh'))
 generator.add(Reshape(img_shape))
 generator.summary()
 
+lrelu = LeakyReLU(alpha=0.01)
+discriminator = Sequential()
+discriminator.add(Flatten(input_shape=img_shape))
+discriminator.add(Dense(128, activation=lrelu))
+discriminator.add(Dense(1, activation='sigmoid'))
+discriminator.summary()
 
+discriminator.compile(loss='binary_crossentropy',
+                      optimizer='adam', metrics=['accuracy'])
+
+gan_model = Sequential()
+gan_model.add(generator)
+gan_model.add(discriminator)
+gan_model.summary()
+gan_model.compile(loss='binary_crossentropy', optimizer='adam')
+
+real = np.ones((batch_size, 1))
+
+fake = np.zeros((batch_size, 1))
+
+for epoch in range(epochs):
+    idx = np.random.randint(0, X_train.shape[0], batch_size)
+    real_imgs = X_train[idx]
+
+    z = np.random.normal(0, 1, (batch_size, noise))
+    fake_imgs = generator.predict(z)
+
+    d_hist_real = discriminator.train_on_batch(real_imgs, real)
+    d_hist_fake = discriminator.train_on_batch(fake_imgs, fake)
+
+    d_loss, d_acc = 0.5 * np.add(d_hist_real, d_hist_fake)
+    discriminator.trainable=False
+
+    z = np.random.normal(0, 1, (batch_size, noise))
+    gan_hist = gan_model.train_on_batch(z, real)
+
+    if epoch % sample_interval == 0:
+        print('%d [D loss: %f, acc.: %.2f%%] [G loss: %f]'%(
+            epoch, d_loss, d_acc * 100, gan_hist))
+        row = col = 4
+        z = np.random.normal(0, 1, (row * col, noise))
+        fake_imgs = generator.predict(z)
+        fake_imgs = 0.5 * fake_imgs + 0.5
+        _, axs = plt.subplots(row, col, figsize=(row, col),
+                             sharey=True, sharex=True)
+        cont = 0
+        for i in range(row):
+            for j in range(col):
+                axs[i, j].imshow(fake_imgs[cont, :, :, 0], cmap='gray')
+                axs[i, j].axis('off')
+                cont += 1
+        path = os.path.join(OUT_DIR, 'img-{}'.format(epoch+1))
+        plt.savefig(path)
+        plt.close()
 
 
 

0a2a1d67c18f2292632ecfd2f2d234d33141e97f
scolpig <scolpig@gmail.com>
2022-04-19 14:14:16 +0900

    Beauty GAN 02

--- exam22_Beauty_GAN.py
+++ exam22_Beauty_GAN.py
@@ -2,6 +2,9 @@ import dlib
 import matplotlib.pyplot as plt
 import matplotlib.patches as patches
 import tensorflow.compat.v1 as tf
+
+
+
 tf.disable_v2_behavior()
 import numpy as np
 
@@ -52,13 +55,83 @@ def align_faces(img):
     faces = dlib.get_face_chips(img, objs,
                 size=256, padding=0.35)
     return faces
-test_img = dlib.load_rgb_image('./imgs/02.jpg')
-test_faces = align_faces(test_img)
-fig, axes = plt.subplots(1, len(test_faces)+1, figsize=(10, 8))
-axes[0].imshow(test_img)
-for i, face in enumerate(test_faces):
-    axes[i + 1].imshow(face)
+# test_img = dlib.load_rgb_image('./imgs/02.jpg')
+# test_faces = align_faces(test_img)
+# fig, axes = plt.subplots(1, len(test_faces)+1, figsize=(10, 8))
+# axes[0].imshow(test_img)
+# for i, face in enumerate(test_faces):
+#     axes[i + 1].imshow(face)
+# plt.show()
+
+sess = tf.Session()
+init_op = tf.group(tf.global_variables_initializer(),
+                   tf.local_variables_initializer())
+sess.run(init_op)
+
+saver = tf.train.import_meta_graph('./models/model.meta')
+saver.restore(sess, tf.train.latest_checkpoint('./models'))
+graph = tf.get_default_graph()
+X = graph.get_tensor_by_name('X:0')
+Y = graph.get_tensor_by_name('Y:0')
+Xs = graph.get_tensor_by_name('generator/xs:0')
+
+def preprocess(img):
+    return img / 127.5 - 1
+def deprocess(img):
+    return (img + 1) / 2
+
+img1 = dlib.load_rgb_image('./imgs/no_makeup/xfsy_0226.png')
+img1_faces = align_faces(img1)
+
+img2 = dlib.load_rgb_image('./imgs/makeup/XMY-136.png')
+img2_faces = align_faces(img2)
+
+fig, axes = plt.subplots(1, 2, figsize=(8, 5))
+axes[0].imshow(img1_faces[0])
+axes[1].imshow(img2_faces[0])
 plt.show()
+
+src_img = img1_faces[0]
+ref_img = img2_faces[0]
+
+X_img = preprocess(src_img)
+X_img = np.expand_dims(X_img, axis=0)
+
+Y_img = preprocess(ref_img)
+Y_img = np.expand_dims(Y_img, axis=0)
+
+output = sess.run(Xs, feed_dict={X:X_img, Y:Y_img})
+output_img = deprocess(output[0])
+
+fig, axes = plt.subplots(1, 3, figsize=(8, 5))
+axes[0].imshow(img1_faces[0])
+axes[1].imshow(img2_faces[0])
+axes[2].imshow(output_img)
+plt.show()
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
